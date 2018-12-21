ce20ad6354c281dc85ca39f22786dd4ab6bd857d
shashank <shashank@timbergrove.com>
2018-12-20 22:46:19 -0500

    Added jenkinsFiles and expose port in dockerfile

---
+++ jenkins/docker_alpine_nginx.groovy
@@ -0,0 +1,36 @@
+#!/usr/bin/groovy
+/*
+This jenkins file will build an image using Alpine linux and Nginx as the web server, 
+Make sure to add the jenkins user to the dockers group 
+sudo usermod -aG docker jenkins 
+sudo service jenkins restart 
+Its very important to restart otherwise the service user will not be able to run docker commands
+*/
+pipeline {
+	agent any
+	stages {
+		stage('delete previous workspace'){
+			steps{
+				echo "delete previous workspace"
+				dir('/var/lib/jenkins/workspace/Docker_alpine_nginx/dockerFiles') {
+					deleteDir()
+				}
+			}
+		}
+		stage('checkout') {
+			steps {
+			    echo "checkout docker files for alpine-nginx"
+                checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'SparseCheckoutPaths', sparseCheckoutPaths: [[path: '/dockerFiles/'], [path: '']]]], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/cshashank/jenkins-k8s/']]])
+			}
+		}
+		stage('build docker image') {
+			steps {
+			    echo "build docker image"
+				dir('/var/lib/jenkins/workspace/Docker_alpine_nginx/dockerFiles') {
+					sh "docker build -t j-alpine-nginx -f nginxDockerFile ."
+				}
+			}
+		}
+
+	}
+}
---
+++ jenkins/jenkins-k8s-angular.groovy
@@ -0,0 +1,59 @@
+#!/usr/bin/groovy
+pipeline {
+	agent any
+
+	stages {
+
+		stage('delete previous workspace'){
+			steps{
+				echo "delete previous workspace"
+				dir('/var/lib/jenkins/workspace/skc-nginx-pipeline/k8s-app') {
+					deleteDir()
+				}
+			}
+		}
+		stage('checkout') {
+			steps {
+			    echo "checkout"
+				checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/cshashank/angular-k8s']]])
+			}
+		}
+		stage('install angular-devkit') {
+			steps {
+			    echo "install angular-devkit"
+				dir('/var/lib/jenkins/workspace/skc-nginx-pipeline/k8s-app') {
+			        sh "npm install --dev-save @angular-devkit/build-angular"
+				}
+			}
+		}	
+		stage('install angular/core') {
+			steps {
+				echo 'install angular/core'
+				dir('/var/lib/jenkins/workspace/skc-nginx-pipeline/k8s-app') {
+			        sh "npm install --dev-save @angular/core"
+				}
+			}
+		}	
+		stage('build angular app') {
+			steps {
+				echo 'Building..'
+				dir('/var/lib/jenkins/workspace/skc-nginx-pipeline/k8s-app') {
+	    	        sh "ng build"
+				}
+			}
+		}
+
+		stage('Test') {
+			steps {
+				echo 'Testing..'
+			}
+		}
+
+		stage('Deploy') {
+			steps {
+				echo 'Deploying....'
+			}
+		}
+
+	}
+}
